83f2b08d3321122edfec384f367ca72c71afb9ab
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-11-05 21:49:45 +0900

    [Setting]

--- src/main.js
+++ src/main.js
@@ -1,21 +1,33 @@
 import Koa from 'koa';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+
+dotenv.config();
+
+const { PORT, MONGO_URI } = process.env;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (e) {
+    console.error(e);
+  }
+}
+connectDB();
 
 const app = new Koa();
 
-const { PORT, MONGO_URI } = process.env;
-mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(e => {
-    console.error(e);
-  });
-
-//response
+// response
 app.use(ctx => {
   ctx.body = 'Hello Koa';
 });
 
-app.listen(3000);
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
